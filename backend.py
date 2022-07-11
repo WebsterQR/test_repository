ff17c5fe599554a90c62aa6f3209cf10dc380e4b
Evgeniy Koverev <50137330+WebsterQR@users.noreply.github.com>
2022-07-11 19:16:44 +0300

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,5 @@
+def result(a, b ,d):
+    if d == 1:
+        return a + b
+    elif d == 2:
+        return a * b
---
+++ frontend.py
@@ -0,0 +1,8 @@
+import backend
+
+print("Что хотите сделать? 1 - сложение, 2 - умножение")
+d = int(input())
+a = int(input("Введите первое число"))
+b = int(input("Введите число"))
+
+print(backend.result(a, b, d))
